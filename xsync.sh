562aa5b9d8c5dfbaaa49dd83c55304b175678bef
songxiao <wangdongsong1229@163.com>
2019-08-26 23:05:03 +0800

    rsync all file

---
+++ xsync.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#1 获取输入参数个数，如果没有参数，直接退出
+pcount=$#
+if((pcount==0)); then
+echo no args;
+exit;
+fi
+
+#2 获取文件名称
+p1=$1
+fname=`basename $p1`
+echo fname=$fname
+
+#3 获取上级目录的绝对路径
+pdir=`cd -P $(dirname $p1); pwd`
+echo pdir=$pdir
+
+#4 获取当前用户名称
+user=`whoami`
+
+#4.1 读取IP列表
+
+
+#5 获取当前用户名称
+curdir=`dirname $0`
+ipfile="ips"
+echo $ipfile
+cat $ipfile | while read line
+do
+#for((host=103; host<105; host++)); do
+	echo ---------hadoop$host--------
+	rsync -rvl $pdir/$fname $user@$line:$pdir
+done
